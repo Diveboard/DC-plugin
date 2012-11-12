516db35f5e60ab440d34ac8ba299a410d39358c6
Pascal <pascal@diveboard.com>
2012-11-12 14:15:37 +0100

    adding script to build release packages under windows

---
+++ mkexewin32.bat
@@ -0,0 +1,15 @@
+set HOME=%HOMEPATH%
+cd %TMP%
+rd /S /Q diveboard-plugin
+"C:\Program Files (x86)\Git\bin\git.exe" clone --depth 1 -- git@diveboard.plan.io:diveboard-db.git "diveboard-plugin"
+
+cd diveboard-plugin
+call firebreath\prep2010.cmd projects build
+call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
+msbuild libdivecomputer\msvc\libdivecomputer.sln /p:Configuration=Release /t:clean,rebuild
+msbuild build\FireBreath.sln /p:Configuration=Release /t:clean,rebuild
+"C:\Program Files (x86)\NSIS\makensis.exe" projects\DiveBoard\installer.nsi
+
+cd %HOMEPATH%
+
+exit
